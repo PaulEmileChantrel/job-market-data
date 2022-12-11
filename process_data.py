215eabddbcb7aebf45dcc4693731861a28884430
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-10 22:22:00 -0500

    finished the streamlit dashboard

--- process_data.py
+++ process_data.py
@@ -2,35 +2,52 @@ import pandas as pd
 import json
 import streamlit as st
 
-
-# Methode to process a df given the csv file_name
-# This will return mutiple df
-@st.cache
-def process_data(file_name='data_analyst_toronto.csv'):
-    # Loading the dataframe file
-    df = pd.read_csv(file_name)
-    print(df.shape[0])
-
+def preformat(file_name):
+    df = pd.read_csv(file_name,index_col=[0])
     # We drop the job duplicates
     df = df.drop_duplicates(subset=['description','title','company_name','via']).reset_index(drop=True)
     print(df.shape[0])
 
+    df['work_type'] = ''
+    # Looking for keywodrs in the df
+    for i in range(df.shape[0]):
+        description = df['description'][i]
+        # We look for hybrid first, since hybrid job descriptions also often use the word remote
+        if 'hybrid' in description.lower():
+            df['work_type'][i] = 'Hybrid'
+        elif 'remote' in description.lower():
+            df['work_type'][i] = 'Remote'
+        else:
+            df['work_type'][i] = 'Office'
+
+    df.to_csv(file_name,index=False)
+    return df
+
+#preformat('data_analyst_toronto.csv')
+
+
+# Methode to process a df given the csv file_name
+# This will return mutiple df
+def process_data(df):
+
     # Group by a columns name and get the number of item for each category
     # Order in descending order to get the top results first
-    def get_top_n_from_col(df,col_name,n):
+    def get_top_n_from_col(df,col_name):
         new_df = df.groupby([col_name]).size().reset_index(name='counts').sort_values(by='counts',ascending=False).reset_index(drop=True)
-        new_df = new_df[:n]
-        print(new_df.head())
+
+        #print(new_df.head())
         return new_df
 
     # Get the top companies
-    companies = get_top_n_from_col(df,'company_name',8)
+    companies = get_top_n_from_col(df,'company_name')
 
     # Get the top job listing places
-    via = get_top_n_from_col(df,'via',8)
+    via = get_top_n_from_col(df,'via')
 
     # Get the top job titles
-    job_title = get_top_n_from_col(df,'title',8)
+    job_title = get_top_n_from_col(df,'title')
+
+    work_df = get_top_n_from_col(df,'work_type')
 
     # Next, we will look for key words in the job desriptions
     key_words = ['SQL','mySQL','Tableau','Python','Power BI', 'Azur','AWS','Hive','Excel','Visual Basic','R','Google Analytics','Omniture','Datorama','ETL','Tririga', 'Ariba', 'Coupa','BigQuery','Snowflake','Redshit','DAX','Java','JavaScript','HTML','CSS','API']
@@ -40,9 +57,6 @@ def process_data(file_name='data_analyst_toronto.csv'):
     for key in key_words:
         key_word_dict[key]=0
 
-    # We also look for keywords regarding the type of work
-    work = {'Hybrid':0,'Remote':0,'Office':0}
-
     # Looking for keywodrs in the df
     for i in range(df.shape[0]):
         description = df['description'][i]
@@ -51,19 +65,11 @@ def process_data(file_name='data_analyst_toronto.csv'):
                 #We increase the count for the key word k by one when we have a match
                 key_word_dict[k]+=1
 
-        # We look for hybrid first, since hybrid job descriptions also often use the word remote
-        if 'hybrid' in description.lower():
-            work['Hybrid']+=1
-        elif 'remote' in description.lower():
-            work['Remote']+=1
-        else:
-            work['Office']+=1
+
 
     key_df = pd.DataFrame.from_dict(key_word_dict, orient='index',columns=['counts']).sort_values(by='counts',ascending=False)
-    print(key_df[:8])
+    #print(key_df[:8])
 
-    work_df = pd.DataFrame.from_dict(work, orient='index',columns=['counts']).sort_values(by='counts',ascending=False)
-    print(work_df.head())
 
     # Finnaly, we look for the different types of scheduals
     schedule_type = {}
@@ -82,7 +88,7 @@ def process_data(file_name='data_analyst_toronto.csv'):
                 schedule_type[job_schedual_type] = 0
 
     schedule_type_df = pd.DataFrame.from_dict(schedule_type, orient='index',columns=['counts']).sort_values(by='counts',ascending=False)
-    print(schedule_type_df.head())
+    #print(schedule_type_df.head())
 
     # We return the different df
     return companies,via,job_title,key_df,work_df,schedule_type_df
--- streamlit_viz.py
+++ streamlit_viz.py
@@ -4,57 +4,118 @@ from process_data import *
 import altair as alt
 import matplotlib.pyplot as plt
 
-companies_df,via_df,job_title_df,key_df,work_df,schedule_type_df=process_data('data_analyst_toronto.csv')
+# Page settings
+st.set_page_config(page_title='Job Market',page_icon=":bar_chart:",layout="wide")
 
-st.write("Here's our first attempt at using data to create a table:")
+# Loading the dataframe file
+df = pd.read_csv('data_analyst_toronto.csv')
+print(df.shape[0])
+
+#df = preformat(df)
+
+
+# -- SIDEBARS --#
+st.sidebar.header("Please filter here :")
+
+work_types = st.sidebar.multiselect(
+    "Select a type of work:",
+    options = df['work_type'].unique(),
+    default = df['work_type'].unique()
+)
+n1 = st.sidebar.slider('Number of companies',min_value=4,max_value=28,value=8)  # 👈 this is a widget
+n2 = st.sidebar.slider('Number of job board',min_value=4,max_value=18,value=8)  # 👈 this is a widget
+n3 = st.sidebar.slider('Number of job titles',min_value=4,max_value=28,value=8)  # 👈 this is a widget
+x = st.sidebar.slider('Number of key words shown',min_value=4,max_value=28,value=8)  # 👈 this is a widget
+
+df_selection = df.query(
+    "work_type == @work_types"
+)
+df_selection.reset_index(drop=True,inplace=True)
 
 
 
+
+# Loading all the df
+companies_df,via_df,job_title_df,key_df,work_df,schedule_type_df=process_data(df_selection)
+companies_df = companies_df[:n1]
+via_df = via_df[:n2]
+job_title_df = job_title_df[:n3]
+
+## -- Page --
+st.title(':bar_chart: Job Market for Data Analys in Toronto')
+st.markdown("##")
+
+
+number_of_jobs = df_selection.shape[0]
+
+left,right = st.columns(2)
+with left:
+    st.subheader(f'Total jobs found : {number_of_jobs}jobs')
+
+st.markdown('---')
+
+
+left,right = st.columns(2)
 # Companies
 companies_df.rename(columns={'company_name':'Company Name'},inplace=True)
-st.write(alt.Chart(companies_df).mark_bar().encode(
-    x=alt.X('Company Name', sort=None),
-    y='counts',))
+with left :
+    st.write('Hiring Companies')
+    st.altair_chart(alt.Chart(companies_df).mark_bar().encode(
+        x=alt.X('Company Name', sort=None),
+        y='counts',).properties(height=400),use_container_width=True)
 
 # Job board
 via_df.rename(columns={'via':'Job board'},inplace=True)
-st.write(alt.Chart(via_df).mark_bar().encode(
-    x=alt.X('Job board', sort=None),
-    y='counts',))
+with right :
+    st.write('Best Job boards')
+    st.altair_chart(alt.Chart(via_df).mark_bar().encode(
+        x=alt.X('Job board', sort=None),
+        y='counts',).properties(height=400),use_container_width=True)
 
-# Job title
-job_title_df.rename(columns={'title':'Job Title'},inplace=True)
-st.write(alt.Chart(job_title_df).mark_bar().encode(
-    x=alt.X('Job Title', sort=None),
-    y='counts',))
+left,right = st.columns(2)
+with left :
+    # Job title
+    st.write('Job Titles')
+    job_title_df.rename(columns={'title':'Job Title'},inplace=True)
+    st.altair_chart(alt.Chart(job_title_df).mark_bar().encode(
+        x=alt.X('Job Title', sort=None),
+        y='counts',).properties(height=500),use_container_width=True)
 
 
-x = st.sidebar.slider('Number of key words shown',min_value=4,max_value=28,value=8)  # 👈 this is a widget
 
 # Key words
 key_df = key_df[:x]
 key_df['Key Words'] = key_df.index
-st.write(alt.Chart(key_df).mark_bar().encode(
-    x=alt.X('Key Words', sort=None),
-    y='counts',))
+with right :
+    st.write('Most Commun Key Words')
+    st.altair_chart(alt.Chart(key_df).mark_bar().encode(
+        x=alt.X('Key Words', sort=None),
+        y='counts',).properties(height=400),use_container_width=True)
 
 
 # Pie chart for work
-work_df['work'] = work_df.index
+left,right = st.columns(2)
 fig1, ax1 = plt.subplots()
-
-ax1.pie(work_df['counts'], labels=work_df['work'], autopct='%1.1f%%',
+fig1.set_facecolor('#00172b')
+patches,texts,autotext= ax1.pie(work_df['counts'], labels=work_df['work_type'], autopct='%1.1f%%',
         shadow=False, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
+for text in texts:
+    text.set_color('white')
+with left:
+    st.write('Types of work')
+    st.pyplot(fig1)
 
 # Pie chart for schedule type
 schedule_type_df['schedual'] = schedule_type_df.index
 fig1, ax1 = plt.subplots()
-
-patches,texts,autotext= ax1.pie(schedule_type_df['counts'], labels=schedule_type_df['schedual'],autopct='%1.1f%%',
+fig1.set_facecolor('#00172b')
+patches,texts,autotexts= ax1.pie(schedule_type_df['counts'], labels=schedule_type_df['schedual'],autopct='%1.1f%%',
         shadow=False, startangle=90)
+for text in texts:
+    text.set_color('white')
+
+
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 # Move a label
@@ -68,4 +129,19 @@ try:
 except:
     pass
 plt.tight_layout()
-st.pyplot(fig1)
+with right:
+    st.write('Types of contracts')
+    st.pyplot(fig1)
+
+
+
+### hide elements
+hide_st_style = """
+    <style>
+    #MainMenu {visibility:hidden;}
+    footer {visibility:hidden;}
+    header {visibility:hidden;}
+    </style>
+"""
+
+st.markdown(hide_st_style,unsafe_allow_html=True)
